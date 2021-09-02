fa57e65fa8ba99d7e5ca0e18db8088e8c8945546
Jeans K. Real <jkrb7@hotmail.com>
2021-09-02 15:05:52 -0600

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Erpnext Delivery
+
+Customizations for Delivery Trip and Delivery Stop
+
+#### License
+
+MIT
---
+++ erpnext_delivery/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Erpnext Delivery",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Erpnext Delivery")
+		}
+	]
